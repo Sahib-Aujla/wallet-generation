f8de747b0bdafeead3316b3e324ea7928ec53afa
Sahib.Aujla <sahibpreet568@gmail.com>
2025-02-09 21:09:44 -0500

    added handleSHow

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+"use client";
+import { useState } from "react";
 //import { getSolanaWallet } from "@/actions/getWallet";
+import { RiArrowDropDownFill, RiArrowDropUpFill } from "react-icons/ri";
 export default function Home() {
   //generate mnemonic if not generated already
-
+  const [isShow, setIsShow] = useState(false);
+  const handleShow = () => {
+    setIsShow((prev) => !prev);
+  };
   return (
     <div className="bg-black flex items-center justify-center w-[100vw] h-[100vh]">
       <div className="w-4/5 h-3/5 text-white">
@@ -15,11 +21,26 @@ export default function Home() {
           <input
             type="text"
             placeholder="Enter your secret phase or leave blank to generate"
-            className="w-3/5 bg-inherit border-slate-300 py-3"
+            className=" border rounded-md border-slate-400 w-3/5 bg-inherit  p-3"
           />
           <button className="bg-white text-black px-6  ml-5 py-3 rounded-md hover:bg-slate-100">
             Generate Wallet
           </button>
+        </div>
+        <div className="border border-slate-400 rounded-md ">
+          <div className="flex justify-between p-3">
+            <h1 className="text-3xl font-semibold">Current Secret Phrase</h1>
+            <div>
+              {!isShow ? (
+                <RiArrowDropDownFill
+                  onClick={handleShow}
+                  className="text-4xl"
+                />
+              ) : (
+                <RiArrowDropUpFill onClick={handleShow} className="text-4xl" />
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </div>
